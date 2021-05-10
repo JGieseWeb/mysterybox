a4096c67daa9176111025e3b679d80d0f5e49ba3
jannik <giese.jannik@web.de>
2021-05-10 19:08:14 +0200

    :tada: Add const readline
    Add const readline

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 import fs from "fs";
 
 console.log(fs.readFileSync("./README.md", "utf-8"));
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`What's your height?`, (name: string) => {
+  console.log(`OH ${name}!`);
+});
+readline.question(`What's your length?`, (name: string) => {
+  console.log(`OH ${name}!`);
+  readline.close();
+});
