b038c2bda05b79b1b6aff50d81c2ab0010579db5
jannik <giese.jannik@web.de>
2021-05-11 15:33:53 +0200

    :sparkles: Add password validation

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,29 @@
-import fs from "fs";
+// import fs from "fs";
 
-console.log(fs.readFileSync("./README.md", "utf-8"));
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// console.log(fs.readFileSync("./README.md", "utf-8"));
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-readline.question(`What's your height?`, (name: string) => {
-  console.log(`OH ${name}!`);
-});
-readline.question(`What's your length?`, (name: string) => {
-  console.log(`OH ${name}!`);
-  readline.close();
-});
+// readline.question(`What's your height?`, (name: string) => {
+//   console.log(`OH ${name}!`);
+// });
+// readline.question(`What's your length?`, (name: string) => {
+//   console.log(`OH ${name}!`);
+//   readline.close();
+// });
+
+import { askForMainPassword } from "./utils/question";
+import { isMainPasswordValid } from "./utils/validation";
+
+const start = async () => {
+  const mainPassword = await askForMainPassword();
+  if (!isMainPasswordValid(mainPassword)) {
+    console.log("is Invalid");
+    start();
+  } else {
+    console.log("is Valid");
+  }
+};
+start();
